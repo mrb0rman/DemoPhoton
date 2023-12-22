5f3f8dabfc8e9c91e40ec22fc48f012d2a05cd2a
mrb0rman <KeinihD@gmail.com>
2023-12-22 13:26:47 +0700

    update

--- Assets/DemoPhoton/Scripts/GunConrtoller.cs
+++ Assets/DemoPhoton/Scripts/GunConrtoller.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class GunConrtoller : MonoBehaviour
 {
     public float force;
     [SerializeField] private Gun[] gun;
-    [SerializeField] private Bullet bullet;
 
     public void Fire()
     {
         for (int i = 0; i < gun.Length; i++)
         {
-            var bulletObject = Instantiate(bullet.gameObject, gun[i].SpawnPountBullet);
+            var bulletObject = PhotonNetwork.Instantiate("Bullet", gun[i].SpawnPountBullet.position, Quaternion.identity, 0);
             bulletObject.GetComponent<Bullet>().Rigidbody.AddForce(bulletObject.transform.up * force);
         }
     }
